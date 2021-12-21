9621b6211011092b641ee4df88b99933f4037c42
Chris Knight <ceknight2020@gmail.com>
2021-12-20 22:49:41 -0600

    initialize repo

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string foo = "bar";
+            var greeter = new Greeter("Chris");
+            Console.WriteLine($"{greeter.SayHello()}");
+        }
+    }
+    public class Greeter
+    {
+        public string Name { get; }
+        public Greeter(string inputName)
+        {
+            Name = inputName;
+        }
+
+        public string SayHello()
+        {
+
+            return $"What's up {Name}";
+        }
+    }
+}
